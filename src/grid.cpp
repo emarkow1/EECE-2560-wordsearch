ac5786f6ab2d27aab99e74311aaf650a84fb111c
= <ethanmarkow@gmail.com>
2026-07-15 15:18:16 -0400

    Implemented grid constructor and CMake

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,3 +1,40 @@
-//
+// grid.cpp
+// Defines member functions of grid class
 
 #include "grid.h"
+
+Grid::Grid(std::string filename) : stored_grid(1, 1)
+{
+    std::ifstream inFile(filename);
+    std::string line;
+
+    if (inFile.is_open())
+    {
+        // Extract n (rows) and m (columns) from the first line of a file
+        std::getline(inFile, line, ' ');
+        int n = stoi(line);
+
+        std::getline(inFile, line);
+        int m = stoi(line);
+
+        stored_grid = matrix<char>(n, m);
+
+        n = 0;
+        
+        // Extract characters from file into internal matrix
+        while (std::getline(inFile, line))
+        {
+            m = 0;
+            std::stringstream ss(line);
+            for (std::string letter; std::getline(ss, letter, ' ');)
+            {
+                stored_grid[n][m] = letter[0];
+                m++;
+            }
+            n++;
+        }
+        inFile.close();
+    } else {
+        std::cerr << "Error Opening File " << filename << std::endl;
+    }
+}
--- src/grid.h
+++ src/grid.h
@@ -1,11 +1,23 @@
-#ifndef GRID
-#define GRID
+#ifndef GRID_H
+#define GRID_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "d_matrix.h"
 
 class Grid
 {
     public:
+    Grid();
+    Grid(std::string filename);
+    char readItem(int i, int j);
 
     private:
+    matrix<char> stored_grid;
+
 };
 
 #endif
