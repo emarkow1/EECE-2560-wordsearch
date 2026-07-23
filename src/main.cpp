58055085558dbab27b2b04a9a24fa560ebd929aa
JakeBerman08 <146116640+JakeBerman08@users.noreply.github.com>
2026-07-23 10:28:12 -0400

    Made more efficient

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,13 @@ int main() {
 }
 
 void findMatches(const Dictionary& dict, const Grid& g) {
+    int index;
     ofstream fout;
     fout.open("output.txt");
     if (!fout) {
         throw "File was not opened successfully";
     }
-
+    
     fout << "word       start point (i, j)    found_index" << endl;
 
     for (int i = 0; i < g.rows(); i++) {
@@ -43,8 +44,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAround = (i + k) % g.rows();
                 word += g.at(loopAround, j);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -53,8 +55,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAround = (i - k + g.rows()) % g.rows();
                 word += g.at(loopAround, j);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -63,8 +66,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAround = (j + k) % g.cols();
                 word += g.at(i, loopAround);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -73,8 +77,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAround = (j - k + g.cols()) % g.cols();
                 word += g.at(i, loopAround);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -85,8 +90,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAroundc = ((j + k) % g.cols());
                 word += g.at(loopAroundr, loopAroundc);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -96,8 +102,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAroundc = ((j - k + g.cols()) % g.cols());
                 word += g.at(loopAroundr, loopAroundc);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -107,8 +114,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAroundc = ((j - k + g.cols()) % g.cols());
                 word += g.at(loopAroundr, loopAroundc);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
@@ -118,8 +126,9 @@ void findMatches(const Dictionary& dict, const Grid& g) {
                 int loopAroundc = ((j + k + g.cols()) % g.cols());
                 word += g.at(loopAroundr, loopAroundc);
                 if (word.length() >= 5) {
-                    if (dict.lookupWord(word) != -1) {
-                        fout << word <<  " (" << i << ", " << j << ") " <<  dict.lookupWord(word) << endl;
+                    index = dict.lookupWord(word);
+                    if (index != -1) {
+                        fout << word <<  " (" << i << ", " << j << ") " <<  index << endl;
                     }
                 }
             }
